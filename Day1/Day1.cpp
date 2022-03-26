e338a3582f05139c2d74d8be9f6f6c6efdfee129
unknown <mathapationkar8@gmail.com>
2022-03-26 23:27:12 +0530

    first problem solved

---
+++ Day1/Day1.cpp
@@ -0,0 +1,13 @@
+// my first program starting with light weight problem
+#include <iostream>
+using namespace std;
+
+int main(){
+          int length;
+          cin>>length;
+
+          int area = length * length;
+          cout<<area;
+
+          return 0;
+}
