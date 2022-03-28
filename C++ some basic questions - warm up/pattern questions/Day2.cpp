086c6422e514c95e8e9e60957af8c04511441833
unknown <mathapationkar8@gmail.com>
2022-03-28 22:45:18 +0530

    day Three binary number problem solved

--- C++ some basic questions - warm up/pattern questions/Day2.cpp
+++ C++ some basic questions - warm up/pattern questions/Day2.cpp
@@ -40,7 +40,7 @@ int main(){
           // int n;
           // cin>>n;
 
-          // for(int i=n; i>=1; i--){
+          // for(int i=n; i>=1;i--){
           //           for(int j=1; j<=i;j++){
           //                     cout<<"*";
           //           }
@@ -67,7 +67,7 @@ int main(){
 
           for(int i=0; i<=n;i++){
                     for(int j=1; j<=i; j++){
-                              cout<<"*"<<" ";
+                              cout<<j<<" ";
                     }
                     cout<<endl;
           }
---
+++ C++ some basic questions - warm up/pattern questions/Day3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+          int n;
+          cin>>n;
+
+
+          // for(int i=0; i<=n; i++){
+          //           for(int j = 1; j<=n+1-i; j++){
+          //                    cout<<j<<" ";
+          //           }
+          //           cout<<endl;
+          // }
+
+          // for(int i=0; i<n; i++){
+          //           for(int j=0; j<=i; j++ ){
+          //                     if((i+j)%2 == 0){
+          //                          cout<<" 1";     
+          //                     }
+          //                     else{
+          //                               cout<<" 0";
+          //                     }
+          //           }
+          //           cout<<endl;
+          // }
+
+          for(int i = 0; i<=n; i++){
+                    for(int j=1; j<=n-i; j++){
+                             cout<<" "; 
+                    }
+                    for(int j=1; j<=n;j++){
+                              cout<<"*";
+                    }
+                    cout<<endl;
+          }
+
+
+
+
+          return 0;
+
+}
