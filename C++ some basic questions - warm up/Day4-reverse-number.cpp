cd29a9d0052bddbec8deb2b00e307889d704bea3
unknown <mathapationkar8@gmail.com>
2022-03-29 20:24:28 +0530

    day 4 reversr number problem solved

--- C++ some basic questions - warm up/Day4-reverse-number.cpp
+++ C++ some basic questions - warm up/Day4-reverse-number.cpp
@@ -3,23 +3,42 @@
 using namespace std;
 
 int main(){
-
-          int n; 
-          cin>>n;
+          //prime number
+          // int n; 
+          // cin>>n;
           
-          bool flag=0;
+          // bool flag=0;
 
-          for(int i=2; i<sqrt(n); i++){
-                  if(n%i==0){
-                     cout<<"Non-Prime"<<endl;  
-                     flag=1;
-                     break;
-                  }
+          // for(int i=2; i<sqrt(n); i++){
+          //         if(n%i==0){
+          //            cout<<"Non-Prime"<<endl;  
+          //            flag=1;
+          //            break;
+          //         }
+
+          // }
+          // if(flag==0){
+          //        cout<<"prime"<<endl;  
+          // }
+
+          //reverse number
+
+          int n;
+          cin>>n;
+
+          int reverse=0;
+          while(n>0){
+                    int lastdigit= n%10;
+                    reverse = reverse*10 + lastdigit;
+                    n=n/10;
 
           }
-          if(flag==0){
-                 cout<<"prime"<<endl;  
-          }
+
+          cout<<reverse;
+
+
+       
+
 
       
           return 0;
